c91da264f988115af3311aa90d4de5f89f7919ba
Piyush <piyushkamble2000@gmail.com>
2023-11-21 17:41:17 +0530

    View, Server side validation, tag helpers

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,6 +17,22 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
     }
 }
